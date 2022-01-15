8a386cdbf785ddff759f5e8600fae3e84f7f108e
Txdeweloper <temurxonsultonov97@gmail.com>
2022-01-15 16:54:01 +0500

    new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import './Style.css';
+import {useState} from 'react';
+import URL from './api/URL';
+import {FcMenu} from 'react-icons/fc'; 
+import MainContainer from './components/MainContainer';
+import VideoList from './components/VideoList';
+
 
 function App() {
+  const [showToogle,setShowToogle] =useState(false);
+  const handleShow =()=>{setShowToogle((m)=>!m)}
+  // console.log(showToogle)
+  // requests
+  const[videos,setVideos]=useState([]);
+  const[selectVideo,setSelectVideo]=useState({id:{}, snippet:{}});
+
+ async function videoSubmit (searchTerm){
+    const{data:{items:videos}}=await URL.get('search',{
+
+      params:{
+        part:'snippet',
+        maxResults:5,
+        key:'AIzaSyB9DeRet2mjIvQAyByi4VtLC1YBR-Mr_Bg',
+        q: searchTerm,
+      }
+    });
+    console.log(videos)
+    setVideos(videos);
+    setSelectVideo(videos[0]);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar onSubmit={videoSubmit}>
+      <FcMenu onClick={handleShow} className='toogle-sidebar'/>
+    </Navbar>
+   <div className="main">
+   <Sidebar showToogle={showToogle}/>
+   <MainContainer videos={selectVideo}/>
+
+   <div className="video-lists">
+   {videos.map(videoItem => <VideoList item={videoItem} key={videoItem.etag}/>)}
+   </div>
+
+   </div>
+    </>
   );
 }
 
